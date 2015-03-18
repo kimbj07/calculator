753b79d49de6bf1d5d10a14a23cccdb59fb5c6da
MilkNRadio <milknradio@gmail.com>
2015-03-18 17:59:40 +0900

    Conflict2 오류부분 수정.

--- main-1.c
+++ main-1.c
@@ -4,17 +4,10 @@ int add(int, int);
 int sub(int, int);
 int div(int, int);
 int multi(int, int);
-<<<<<<< HEAD
- 
-int main(int argc, char* argv[])
-{
-	int g, h, i;
-=======
 
 int main(int argc, char* argv[])
 {
 	int a, b, c;
->>>>>>> feature/iss1-create-skeleton
 	char operator;
 	
 	printf("Please enter a expression");
@@ -37,12 +30,7 @@ int main(int argc, char* argv[])
 	}
 	
 	printf("result is %d.\n", c);
-	
-<<<<<<< HEAD
-=======
-	// for push test.
-	
->>>>>>> feature/iss1-create-skeleton
+
 	return 0;
 }
  
